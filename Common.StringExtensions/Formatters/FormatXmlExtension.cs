4729cf44ab4abd62e4bcec45b3acbc6ab0169242
Christopher Dorst <chris@dorst.email>
2017-12-06 05:36:15 -0600

    Adding "".FormatXml() extension

---
+++ Common.StringExtensions/Formatters/FormatXmlExtension.cs
@@ -0,0 +1,19 @@
+﻿using static System.Xml.Linq.XDocument;
+
+namespace Common.StringExtensions.Formatters
+{
+    public static class FormatXmlExtension
+    {
+        public static string FormatXml(this string instance)
+        {
+            try
+            {
+                return Parse(instance).ToString();
+            }
+            catch
+            {
+                return instance;
+            }
+        }
+    }
+}
