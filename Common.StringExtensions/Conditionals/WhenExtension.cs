bcc171c2b4aa22a07e417759805924dca031b6d8
Christopher Dorst <chris@dorst.email>
2017-12-06 05:35:59 -0600

    Adding "OnlyShowMe".When(true) extension

---
+++ Common.StringExtensions/Conditionals/WhenExtension.cs
@@ -0,0 +1,8 @@
+﻿namespace Common.StringExtensions.Conditionals
+{
+    public static class WhenExtension
+    {
+        public static string When(this string instance, bool condition)
+            => condition ? instance : string.Empty;
+    }
+}
